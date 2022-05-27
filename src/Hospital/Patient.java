4e750c5cfefacc1fb656930b3169aed47371b1ba
osama <oaqutait@gmail.com>
2022-05-27 21:20:40 +0300

    Updated test osama

--- src/Hospital/Patient.java
+++ src/Hospital/Patient.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Date;
-//test
+//testttt
 public class Patient {
     private int recordNumber;
     private int lengthOfStay;
